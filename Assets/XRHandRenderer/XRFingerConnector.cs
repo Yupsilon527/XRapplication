e6100287d2364adeae6b32458ad9a2236e88ceb3
eespee <paulsendroiu@gmail.com>
2022-05-02 18:44:33 +0300

    SmoothMove

--- Assets/XRHandRenderer/XRFingerConnector.cs
+++ Assets/XRHandRenderer/XRFingerConnector.cs
@@ -18,10 +18,13 @@ public class XRFingerConnector : MonoBehaviour
     {
         rBody = GetComponent<Rigidbody>();
     }
-    void Update()
+    void FixedUpdate()
     {
-        SnapToPos();
-    //    SmoothMove(MovementTime);
+        //SmoothMove(MovementTime);
+        if (MovementTime>0)
+        PhysicsPos();
+        else
+            SnapToPos();
     }
     private void OnValidate()
     {
@@ -37,8 +40,10 @@ public class XRFingerConnector : MonoBehaviour
     void SmoothMove(float time)
     {
         Vector3 deltaPos = (Master.transform.position - Slave.transform.position);
-        LeanTween.move(gameObject, Master.transform.position - deltaPos / 2f, time);
-        LeanTween.scale(gameObject, new Vector3(FingerThickness, deltaPos.magnitude * .5f, FingerThickness), time);
+        //LeanTween.move(gameObject, Master.transform.position - deltaPos / 2f, time);
+        transform.position = Master.transform.position - deltaPos / 2f;
+        //LeanTween.scale(gameObject, new Vector3(FingerThickness, deltaPos.magnitude * .5f, FingerThickness), time);
+        transform.localScale = new Vector3(FingerThickness, deltaPos.magnitude * .5f, FingerThickness);
 
         Quaternion startRot = transform.rotation;
 
@@ -46,24 +51,48 @@ public class XRFingerConnector : MonoBehaviour
         Quaternion desiredRot = transform.rotation;
         transform.rotation = startRot;
 
-        LeanTween.rotate(gameObject, desiredRot.eulerAngles, time);
-
+        //LeanTween.rotate(gameObject, desiredRot.eulerAngles, time);
+        transform.rotation = desiredRot;
     }
     void PhysicsPos()
     {
         Vector3 vA = Master.transform.position;
         Vector3 vB = Slave.transform.position;
         Vector3 desiredPos = (vA + vB) * .5f;
-        rBody.velocity = (desiredPos-transform.position) * MoveSpeed;
+
+        Vector3 velocity = (desiredPos - transform.position);
+        if (velocity.sqrMagnitude < MoveSpeed * MoveSpeed)
+        {
+            rBody.position = desiredPos;
+            rBody.velocity *= 0;
+        }
+        else
+            rBody.velocity = velocity / MovementTime ;
 
 
         //Vector3 axis = Vector3.Cross(vB, vA).normalized;
         //float angle = Mathf.Acos(Vector3.Dot(vA,vB)) * Mathf.Rad2Deg;
 
-        Quaternion desiredRot =  Quaternion.LookRotation( transform.forward, vA - vB);
+        /*Quaternion desiredRot =  Quaternion.LookRotation( transform.forward, vA - vB);
         desiredRot*= Quaternion.Inverse(transform.rotation);
         
         desiredRot.ToAngleAxis(out float angle, out Vector3 axis);
-        rBody.angularVelocity = axis * angle * Mathf.Deg2Rad * AngSpeed;
+        rBody.angularVelocity = axis * angle * Mathf.Deg2Rad * AngSpeed;*/
+
+        Quaternion rStart = transform.rotation;
+        transform.up = vB-vA;
+        Quaternion rFinal = transform.rotation;
+        transform.rotation = rStart;
+
+        //Quaternion deltaRot = ;
+        /*if (Mathf.Abs(deltaRot.x) < AngSpeed && Mathf.Abs(deltaRot.y) < AngSpeed && Mathf.Abs(deltaRot.z) < AngSpeed)
+        {
+            rBody.rotation = Quaternion.Euler(rB);
+            rBody.angularVelocity = Vector3.zero;
+        }
+        else */
+        //rBody.angularVelocity = deltaRot.eulerAngles;
+        transform.rotation = Quaternion.RotateTowards(rStart, rFinal, AngSpeed);
+
     }
 }
